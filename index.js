e33c7d0cf6abc211409682605a1cdeffec7d5106
Milton Mamani <miltin.mamani010@gmail.com>
2025-08-06 22:30:57 -0300

    agregamos la config del server

---
+++ index.js
@@ -0,0 +1,6 @@
+import Server from "./server/config.js";
+
+const newServer = new Server();
+newServer.listen();
+
+// agregar las rutas 
--- server/config.js
+++ server/config.js
@@ -1,12 +1,33 @@
-// npm install express --save
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-// npm i cors
+export default class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+    this.middlewares();
+  }
 
-// npm i morgan
+  // AGREGAR METODOS
+  // middlewares
+  middlewares() {
+    this.app.use(cors()); //permite tener conecciones remotas
+    this.app.use(express.json()); //interpreta los datos JSON que llega en la solicitud
+    this.app.use(morgan("dev")); //muestra en consola las peticiones que llegan al servidor
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    this.app.use(express.static(`${__dirname}/../public`)); //sirve archivos estáticos desde la carpeta public);
+    console.log(`${__dirname}/../public`);
+  }
 
-// npm install mongoose
-
-
-
-
-
+  // escuchar el puerto
+  listen() {
+    this.app.listen(this.port, () => {
+      console.info(
+        `el servidor se esta ejecutando en: http://localhost:${this.port}`
+      );
+    });
+  }
+}
